e63f0fd595f5f7d9b0fc275a187addacbe054072
Fabio <fs.embarcados@gmail.com>
2019-11-17 17:57:19 -0300

    Primeiro commit

---
+++ 00_Exemplo/src/main.cpp
@@ -0,0 +1,26 @@
+/*********************************************************
+* Exemplo Blink LED - ESP32
+* Pisca o LED da placa em intervalos de 1 segundo
+* Exemplo para entender o fluxo de desenvolvimento no PIO
+* Por: Fábio Souza
+*********************************************************/
+
+/*inclusão da Biblioteca Arduino*/
+#include <Arduino.h>
+
+/*Definição do Pino do LED*/
+#define LED 2
+
+/*Função setup - é chamada apenas uma vez na inicialização */
+void setup() {
+  /*Configura pino do LED como saída*/
+  pinMode(LED,OUTPUT);
+}
+
+/*Função loop - fica em loop*/
+void loop() {
+  digitalWrite(LED,HIGH);
+  delay(1000);
+  digitalWrite(LED,LOW);
+  delay(1000);
+}
---
+++ 01_Exemplo/src/main.cpp
@@ -0,0 +1,53 @@
+/*********************************************************
+* Exemplo Hello RTOS
+* Pisca o LED da placa em intervalos de 200 milisegundo e
+* Envia a mensagem "Oi" a cada 10 segundos
+* Exemplo para verificar o funcionamento de um RTOS
+* Por: Fábio Souza
+*********************************************************/
+
+/*inclusão da Biblioteca Arduino*/
+#include <Arduino.h>
+
+/*inclusão das Biblioteca do FreeRTOS*/
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+/*Definição do Pino do LED*/
+#define LED 2
+
+/* Protótipo da Task*/
+void vTaskBlink(void *pvParameters ); 
+
+void setup() {
+
+  Serial.begin(9600);
+  
+  xTaskCreate(
+     vTaskBlink
+    ,  "TaskBlink"   // Nome da Task
+    ,  1024          // Stack Size, não se preocupe com esse valor agora. Vamos estudar mais pra frente
+    ,  NULL          // parametro passado para a task
+    ,  2             // Prioridade da task
+    ,  NULL);        // handle da task
+}
+
+//Função Loop
+void loop() { 
+  vTaskDelay(pdMS_TO_TICKS(100000));
+  Serial.println("Oi");
+}
+
+//Task Blink LED
+void vTaskBlink(void *pvParameters ){
+
+  (void) pvParameters;
+
+  pinMode(LED,OUTPUT);
+
+  while(1){
+    digitalWrite(2,!digitalRead(2));
+    vTaskDelay(pdMS_TO_TICKS(200));
+  }
+}
+
